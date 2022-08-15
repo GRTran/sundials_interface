5becc75ebc877b4614a88441e0b205c69eccbdab
grtran <greg.jones@live.co.uk>
2022-08-15 08:21:55 +0100

    no printing

--- sundials_solve.f90
+++ sundials_solve.f90
@@ -316,10 +316,10 @@ module sundials_solve
 
         ! convert the input vector to the sunvector type
         iflag = FCVode(this%cvode_mem, end_time, this%solution_vector, arr_curr_time, CV_NORMAL)
-        if(iflag /= 0) then
-            write(*,*) iflag
-            write(*,*) 'coming out of solver'
-        endif
+        ! if(iflag /= 0) then
+        !     write(*,*) iflag
+        !     write(*,*) 'coming out of solver'
+        ! endif
 
         curr_time = arr_curr_time(1)
     end subroutine
